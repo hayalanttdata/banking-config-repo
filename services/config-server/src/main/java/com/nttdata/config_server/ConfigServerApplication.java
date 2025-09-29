e510048ade72f0e32dfe9c07c9ad16318693bf12
hayalanttdata <h0392555@gmail.com>
2025-09-28 23:50:40 -0500

    cambios y ajustes de configuracion

--- services/config-server/src/main/java/com/nttdata/config_server/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/nttdata/config_server/ConfigServerApplication.java
@@ -1,11 +1,11 @@
-package com.nttdata.config_server;
+package com.nttdata.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
